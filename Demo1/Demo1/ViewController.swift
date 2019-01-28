a3d45628a66c1f5806eb3943dbbd84849b53c8ed
京鑫 <engos@me.com>
2019-01-28 22:10:16 +0800

    Initialization

---
+++ Demo1/Demo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by kata ding on 2019/1/28.
+//  Copyright © 2019 kata ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
